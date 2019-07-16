e1139b7e9f7928e5b7f2b4c256f8ee5542dbc03e
chinweike <chilearnsap@users.noreply.github.com>
2019-07-16 18:29:02 +0100

    ALC4.0
    5 Day Challeneg- Android

--- app/src/main/java/com/example/androidchallenge/Activity2.java
+++ app/src/main/java/com/example/androidchallenge/Activity2.java
@@ -1,4 +1,13 @@
 package com.example.androidchallenge;
 
-class Activity2 {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
 }
